5d5f41938d44b82c0f13f07eadf70977a56256ef
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-08-21 00:20:33 -0300

    refactor: clean up Header component structure and update logo image source

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 type HeaderProps = {
-    title?: string;
-    buttonText?: string;
+  title?: string;
+  buttonText?: string;
 };
 
-const Header: React.FC<HeaderProps> = ({
-    title = "Valorim",
-    buttonText = "Entrar",
-}) => {
-    return (
-        <header>
-            <nav className="flex items-center justify-between w-full h-16 px-4 bg-white text-primaryGreen">
-                <div className="flex items-center gap-4">
-                    <Image
-                        alt="valorim-icone"
-                        src="/images/valorim logo.png"
-                        width={37}
-                        height={37}
-                    />
-                    <span className="font-bold text-[24px]">{title}</span>
-                </div>
-                <button className="px-2 py-1 font-bold text-black border-primaryGreen border-3 w-26 rounded-xl text-md">
-                    {buttonText}
-                </button>
-            </nav>
-        </header>
-    );
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header>
+      <nav className="mt-[1rem] flex items-center justify-between w-full h-16 px-4 bg-transparent ">
+        <div className="flex items-center gap-4">
+          <Image
+            alt="valorim-icone"
+            src="/images/valorim_logo.png"
+            width={37}
+            height={37}
+          />
+          <h2 className="font-bold text-[24px] text-primaryGreen">Valorim</h2>
+        </div>
+        <Link
+          href="/LoginPage"
+          className="px-2 py-1 font-bold text-black border-primaryGreen border-3 w-26 rounded-xl text-md"
+        >
+          Entrar
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
