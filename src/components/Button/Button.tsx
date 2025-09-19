009ee99bcbd797e7e52c80ed69685ed1a5a8253d
Pablo JDev <pablodosantos2007@gmail.com>
2025-09-19 17:47:29 -0300

    feat: add image facebook and google icon in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import {ButtonProps} from "./ButtonProps"
-
-const Button: React.FC <ButtonProps> = ({title, href}) => {
+import {ButtonProps} from "../Button/ButtonProps"
+import Image from "next/image";
+const Button: React.FC <ButtonProps> = ({title, href, imageSrc="/images/facebook_icon.png"}) => {
     return (
         <a href={href}>
-        <button className="w-[154px] h-[58px] border rounded-xl bg-backgroundClaro hover:bg-gray-200 cursor-pointer">
-                <h1 className="font-bold text-xl">
+        <button className="flex justify-center items-center gap-2 w-[154px] h-[58px]  border rounded-xl bg-backgroundClaro hover:bg-gray-200 cursor-pointer">
+            <Image className="object-contain" src={imageSrc} alt={""} width={24} height={24}/>
+                <span className="font-bold text-xl">
                     {title}
-                </h1>
+                </span>
         </button>
         </a>
     )
 }
 
 export default Button;
+//mudei o h1 para span
