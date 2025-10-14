1d704840720ff27ffe54891eff1cf393afe62b07
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-14 10:04:43 -0300

    feat: text responsiveness

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { TextProps } from "./TextProps";
 
-const Text: React.FC <TextProps> = ({h1, h2, p}) => {
+const Text: React.FC <TextProps> = ({h1, h2, p, className}) => {
     return(
-        <div className="flex flex-col items-center justify-center">
-        <h1 className="font-bold text-4xl">{h1}</h1>
+        <div className={`flex flex-col items-center justify-center ${className}`}>
+        <h1 className={`font-bold text-4xl lg:text-3xl mt-5 ${className}`}>{h1}</h1>
         <h2 className="font-bold text-2xl">{h2}</h2>
         <p className="font-bold mb-3 ml-8">{p}</p>
         </div>
--- src/components/Text/TextProps.d.ts
+++ src/components/Text/TextProps.d.ts
@@ -2,4 +2,5 @@ export type TextProps = {
     h1: string;
     h2: string;
     p: string;
+    className?: string;
 };
