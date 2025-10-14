82de0cd7bac36c675e63306cf4630de8be0bc355
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-14 13:27:33 -0300

    style: addition of the color beige

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customBlue: '#1e40af',
-        primaryGreen: '#2ecc71', // Verde
-        secondaryColor: '#2c3e50', // Grafite
-        alertColor: '#f1c40f', // Âmbar
-        alertError: '#e74c3c', // Vermelho
-        alertSuccess: '#27ae60', // Verde escuro
-        fundoEscuro: '#1e272e', // Azul Marinho Escuro
-        backgroundClaro: '#f5f6fa', // Cinza Claro
-        textClaro: '#333333', // Cinza Escuro
+        customBlue: "#1e40af",
+        primaryGreen: "#2ecc71", // Verde
+        secondaryColor: "#2c3e50", // Grafite
+        alertColor: "#f1c40f", // Âmbar
+        alertError: "#e74c3c", // Vermelho
+        alertSuccess: "#27ae60", // Verde escuro
+        fundoEscuro: "#1e272e", // Azul Marinho Escuro
+        backgroundClaro: "#f5f6fa", // Cinza Claro
+        textClaro: "#333333", // Cinza Escuro
+        secondaryBackground: "#F3F0E7", // bege
       },
     },
   },
